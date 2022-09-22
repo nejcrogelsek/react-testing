24c6f5e25aa32e3e4f0cc422915997e2f77dcb00
nejcrogelsek <nejcrogelsek0@gmail.com>
2022-09-22 16:09:49 +0200

    Custom render function

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import AppProviders from "../../providers/app-providers";
+import { render, screen } from "../../test-utils";
 import MuiMode from "./mui-mode";
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
 
     const headingElement = screen.getByRole("heading", {
       name: /mode/i,
